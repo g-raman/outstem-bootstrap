ed42c798ece203a98422dd2ea5b81b3a0b1995c7
Raman Gupta <gr.gupta.raman@gmail.com>
2024-07-26 13:25:49 -0400

    fix: error msg

--- reset.ps1
+++ reset.ps1
@@ -76,3 +76,4 @@ Write-Header "Clearing SSH keys"
 $homePath = [System.Environment]::GetFolderPath("UserProfile")
 $sshPath = Join-path -Path $homePath -ChildPath ".ssh\*"
 Remove-Item -Path $sshPath -Force -Recurse
+Write-MessageIfError "Something went wrong when removing ssh keys" "SSH keys removed successfully" 
